92e4999ebedeaefb4cc18cd84b32c10994590358
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-19 12:02:36 -0600

    Added characters to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword(length) {
-  var password = generatePassword();
+  var password = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890"();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
