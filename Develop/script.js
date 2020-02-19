aa7dacc7037817f43ea8ad87c0cd66731d2aa86d
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-19 12:01:41 -0600

    Added length to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(length) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
