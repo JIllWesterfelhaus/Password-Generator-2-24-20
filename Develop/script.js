b4f67a49cb34783e24cdd6dfaf2257bbf2a88866
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-20 10:16:08 -0600

    Added prompts for number of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,19 @@
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"] 
+var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
 var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var specialCharacters = ["`", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "[", "]", ":", ";", ",", ".", "<", ">", "?", "/"]
+
+prompt ("How many characters do you want in your password?")
+var numberCharacters = prompt ("How many characters do you want in your password?");
+if (numberCharacters) {
+    alert ("You want " + numberCharacters + "in your password.")
+}
+
+
+
+
 function generatePassword() { //write code here to generate password from what client wants//
 
 }
@@ -11,7 +21,7 @@ function generatePassword() { //write code here to generate password from what c
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); 
 
   passwordText.value = password;
 
