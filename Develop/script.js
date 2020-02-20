0efd09f41f36ca6d6a184b2554ab2dbadfae6f67
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-19 18:22:38 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,22 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = ["q", "w",]
+var upperCase = []
+var numbers = [1,2,3,4,5,6,7,8,9,0]
+var specialCharacters = []
+
+function generatePassword() {
+  
+}
+
 // Write password to the #password input
-function writePassword(length) {
-  var password = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890"();
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
-For(var i = 0; i < writePassword.length; i++) {
-  passwordText += password.chartAt(Math.floor(Math.random() * length));
+
 }
 
 // Add event listener to generate button
-
-//if? alert?
 generateBtn.addEventListener("click", writePassword);
