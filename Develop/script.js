99c70d522f75b6a7b3472ff1ff3faca400d9bbd1
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-20 09:50:43 -0600

    Added special characters

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"] 
 var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-var specialCharacters = []
-
+var specialCharacters = ["`", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "[", "]", ":", ";", ",", ".", "<", ">", "?", "/"]
 function generatePassword() { //write code here to generate password from what client wants//
 
 }
