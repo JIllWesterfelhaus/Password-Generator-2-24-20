92acd0f1833fb23610e97ffcc9ee610e4cb25f32
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-20 09:37:57 -0600

    Added uppercase letters

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 var generateBtn = document.querySelector("#generate");
 
 var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"] 
-var upperCase = []
+var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var specialCharacters = []
 
