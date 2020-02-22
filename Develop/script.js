a906ddc79d6212557d08552c651f1fe3442f80a7
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-22 14:09:01 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -7,23 +7,17 @@ var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D"
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var specialCharacters = ["`", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "[", "]", ":", ";", ",", ".", "<", ">", "?", "/"]
 
-
 var numberCharacters = prompt("How many characters do you want in your password?  It must be between 8 and 128 characters.");
-
 
 //Stop here if less than 8 characters or more than 128 characters requested.//
 
 var confirmLowerCase = confirm("Do you want lowercase letters in your password?");
 
-
 var confirmUpperCase = confirm("Do you want uppercase letters in your password?");
-
 
 var confirmNumbers = confirm("Do you want numbers in your password?");
 
-
 var confirmSpecialCharacters = confirm("Do you want special characters in your password?");
-
 
 //Stop here if no character types or numbers are requested.//
 
@@ -52,7 +46,6 @@ function generatePassword() {
     for (var i = 0; i < numberCharacters; i++) {
         password += Math.floor()
     }
-
 
     //write code here to generate password from what client wants.  Math random, math floor?//
 }
