2979c41392a1360d2fbab4f89ad3c9cfc71737d0
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-22 14:06:58 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -8,29 +8,26 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var specialCharacters = ["`", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "[", "]", ":", ";", ",", ".", "<", ">", "?", "/"]
 
 
-var numberCharacters = prompt ("How many characters do you want in your password?  It must be between 8 and 128 characters.");
+var numberCharacters = prompt("How many characters do you want in your password?  It must be between 8 and 128 characters.");
 
 
 //Stop here if less than 8 characters or more than 128 characters requested.//
 
-var confirmLowerCase = confirm ("Do you want lowercase letters in your password?");
+var confirmLowerCase = confirm("Do you want lowercase letters in your password?");
 
-    
-    
-var confirmUpperCase = confirm ("Do you want uppercase letters in your password?");
 
-     
+var confirmUpperCase = confirm("Do you want uppercase letters in your password?");
 
-var confirmNumbers = confirm ("Do you want numbers in your password?");
 
-    
+var confirmNumbers = confirm("Do you want numbers in your password?");
 
-var confirmSpecialCharacters = confirm ("Do you want special characters in your password?");
+
+var confirmSpecialCharacters = confirm("Do you want special characters in your password?");
 
 
 //Stop here if no character types or numbers are requested.//
 
-function generatePassword() {   
+function generatePassword() {
 
     megaArray = []
 
@@ -52,21 +49,21 @@ function generatePassword() {
 
     console.log(megaArray)
     var password = ""
-    for(var i = 0; i < numberCharacters; i++) {
-        password += Math.floor ()
+    for (var i = 0; i < numberCharacters; i++) {
+        password += Math.floor()
     }
 
 
     //write code here to generate password from what client wants.  Math random, math floor?//
 }
-generatePassword ()
+generatePassword()
 
 // Write password to the #password input//
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password"); 
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 }
 
 // Add event listener to generate button//
